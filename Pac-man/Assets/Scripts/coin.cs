df0dbe446553981e60f45684feaf4e89d2f8cc5a
AzkaKurniawanAlfathu <azka.0528@students.amikom.ac.id>
2018-10-03 00:11:55 +0700

    pakman baru
    score,move, destroy point

---
+++ Pac-man/Assets/Scripts/coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour {
+    pacman playe;
+
+    // Use this for initialization
+    void Start () {
+        playe = GameObject.Find("player").GetComponent<pacman>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D (Collider2D other){
+		if(other.transform.tag == "Player"){
+			Destroy(gameObject);
+			playe.coin+= 50;
+		}
+	}
+}
---
+++ Pac-man/Assets/Scripts/pacman.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pacman : MonoBehaviour {
+
+    //score
+    public Text infoCoin;
+    public int coin;
+    //end score
+    public float speed = 4.0f;
+    private Vector2 arah = Vector2.zero;
+    
+	// Use this for initialization
+	void Start () {
+		infoCoin = GameObject.Find("UIcoin").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Pinput();
+        move();
+        koin();
+	}
+
+    void Pinput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            arah = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){          
+            arah = Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)){           
+            arah = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)){            
+            arah = Vector2.down;
+        }
+    }
+
+    void move()
+    {
+        transform.localPosition += (Vector3)(arah * speed) * Time.deltaTime;
+    }
+    void koin(){
+        infoCoin.text = "score : " + coin.ToString();
+    }
+}
